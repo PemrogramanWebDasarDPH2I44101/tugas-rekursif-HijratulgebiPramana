28f92d7388a694308915b30cec7d94e986de2de9
HIJRATULGEBI PRAMANA <42725375+HijratulgebiPramana@users.noreply.github.com>
2018-09-10 19:35:10 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,18 +1,31 @@
 <?php
-function rekursifBintang1($p) {
+function rekursifBintang($p) {
   $p--;
-  if ($p > 0)rekursifBintang1($p);
+  if ($p > 0)rekursifBintang($p);
 	echo "*";
 }
 function rekursifBintang2($p){
-  rekursifBintang1($p);
+  rekursifBintang($p);
   echo "<br>";
   $p--;
   if($p>0)
     rekursifBintang2($p);
 }
-	rekursifBintang2(7);
-echo "<br>=========================================== <br><br>";
+	rekursifBintang2(6);
+echo "<br>------------------------------------- <br><br>";
 
+function rekursifAngka($q, $r){
+  echo $q;
+    $q++;
+  		if($q <=$r)rekursifAngka($q, $r);  
+}
+function rekursifAngka2($q, $r){
+  rekursifAngka(1, $q);
+   echo "<br>";
+  	$q++;
+  		if($q <=$r)rekursifAngka2($q, $r);  
+}
+rekursifAngka2(1,6);
 
 ?>
+
